ec233c2a79a6863de55868e95bd8e80071c57e89
Oscar Finnsson <oscar.finnsson@gmail.com>
2011-04-16 12:40:59 +0200

    Updated .cabal and tests for GHC 7

--- src/Text/JSON/TestQQ.hs
+++ src/Text/JSON/TestQQ.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -fglasgow-exts -XTemplateHaskell -XQuasiQuotes #-}
+{-# OPTIONS_GHC -XDeriveDataTypeable -XTemplateHaskell -XQuasiQuotes #-}
 module Text.JSON.TestQQ where
 
 import Text.JSON.QQ
@@ -22,58 +22,60 @@ import Data.Char
 
 import Language.Haskell.TH 
 
-main = defaultMain [tests]
+main = $defaultMainGenerator
 
-tests = $testGroupGenerator
+-- main = defaultMain [tests]
+
+-- tests = $testGroupGenerator
 
 case_get_QQ_to_compile = do
-  let actual = [$jsonQQ| {foo: "ba r.\".\\.r\n"} |]
+  let actual = [jsonQQ| {foo: "ba r.\".\\.r\n"} |]
       expected = JSObject $ toJSObject [("foo", JSString $ toJSString "ba r.\\\".\\\\.r\\n")]
   expected @=? actual
 
 case_arrays = do
-  let actual = [$jsonQQ| [null,{foo: -42}] |]
+  let actual = [jsonQQ| [null,{foo: -42}] |]
       expected = JSArray [JSNull, JSObject $ toJSObject [("foo", JSRational False (-42 % 1))] ]
   expected @=? actual
 
 case_code = do
-  let actual = [$jsonQQ| [null,{foo: <|x|>}] |]
+  let actual = [jsonQQ| [null,{foo: <|x|>}] |]
       expected = JSArray [JSNull, JSObject $ toJSObject [("foo", JSRational False (42 % 1))] ]
       x = 42 :: Integer
   expected @=? actual
 
 case_true = do
-  let actual = [$jsonQQ| [true,false,null] |]
+  let actual = [jsonQQ| [true,false,null] |]
       expected = JSArray [JSBool True, JSBool False, JSNull]
   expected @=? actual
 
 case_json_var = do
-  let actual = [$jsonQQ| [null,{foo: <<x>>}] |]
+  let actual = [jsonQQ| [null,{foo: <<x>>}] |]
       expected = JSArray [JSNull, JSObject $ toJSObject [("foo", JSRational False (42 % 1))] ]
       x = toJSON ( 42 :: Integer)
   expected @=? actual
 
 case_foo = do
-  let actual = [$jsonQQ| <|foo|> |]
+  let actual = [jsonQQ| <|foo|> |]
       expected = JSObject $ toJSObject [("age", JSRational False (42 % 1) ) ]
       foo = Bar 42
   expected @=? actual
 
 case_quoted_name = do
-  let actual = [$jsonQQ| {"foo": "bar"} |]
+  let actual = [jsonQQ| {"foo": "bar"} |]
       expected = JSObject $ toJSObject [("foo", JSString $ toJSString "bar")]
       foo = "zoo"
   expected @=? actual
 
 case_var_name = do
-  let actual = [$jsonQQ| {$foo: "bar"} |]
+  let actual = [jsonQQ| {$foo: "bar"} |]
       expected = JSObject $ toJSObject [("zoo", JSString $ toJSString "bar")]
       foo = "zoo"
   expected @=? actual
 
 case_multiline = do
   let actual =
-        [$jsonQQ|
+        [jsonQQ|
           [   {
             user: 
               "Pelle"},
@@ -83,20 +85,20 @@ case_multiline = do
   expected @=? actual
 
 case_simple_code = do
-  let actual = [$jsonQQ| { foo: <| foo |> } |]
+  let actual = [jsonQQ| { foo: <| foo |> } |]
       expected = JSObject $ toJSObject [("foo", JSString $ toJSString "zoo")]
       foo = "zoo"
   expected @=? actual
 
 case_semi_advanced_code = do
-  let actual = [$jsonQQ| { foo: <| foo + 45 |> } |]
+  let actual = [jsonQQ| { foo: <| foo + 45 |> } |]
       expected = JSObject $ toJSObject [("foo", JSRational False (133 % 1))]
       foo = 88 :: Integer
   expected @=? actual
 
 
 case_semi_advanced_char = do
-  let actual = [$jsonQQ| { name: <| map toUpper name |> } |]
+  let actual = [jsonQQ| { name: <| map toUpper name |> } |]
       expected = JSObject $ toJSObject [("name", JSString $ toJSString "PELLE")]
       name = "Pelle" 
   expected @=? actual
